6aaa2137791ee6d087fe3dcc38ab09011b13b540
Abhishek P <113097985+abhiiishek-10@users.noreply.github.com>
2023-12-07 19:05:09 +0530

    Add: Fonts added

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
+    $('.section.intro').css('width', `${window.innerWidth - $('.bands').width()}px`)
 
     const sections = gsap.utils.toArray('section');
+    const wrapper = document.querySelector(".wrapper");
+    let wrapperWidth = 0;
+    sections.forEach(sec => {
+        wrapperWidth += $(sec).width();
+    })
+    // console.log(wrapperWidth);
+
     let scrollTween = gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
+        // xPercent: -100 * (sections.length - 1),
+        x: `-${wrapperWidth - window.innerWidth}px`,
         ease: 'none',
         scrollTrigger: {
             trigger: '.wrapper',
@@ -15,7 +23,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // snap: 1 / (sections.length - 1),
             start: 'top top',
             endTrigger: $('.wrapper').width(),
-            end: 3000,
+            markers: true,
+            end: () => "+=" + (wrapper.offsetWidth - innerWidth)
         }
     })
 
@@ -25,10 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const bands = gsap.utils.toArray('.band');
     let offset = 0;
     let reversedOffset = $('.band').length * $('.band').width();
-
+    let bandTriggers = [];
     bands.forEach(band => {
-        gsap.to(band, {
-            ease: 'none',
+        bandTriggers.push(gsap.to(band, {
+            ease: 'linear',
             xPercent: -(band.getBoundingClientRect().left - offset),
             scrollTrigger: {
                 containerAnimation: scrollTween,
@@ -42,34 +51,40 @@ document.addEventListener('DOMContentLoaded', () => {
                 // end: 3000,
                 markers: true
             }
-        })
+        }))
         offset += $('.band').width();
         reversedOffset -= $('.band').width();
-
     })
-    let panelsSection = document.querySelector("#panels"),
-        panelsContainer = document.querySelector(".wrapper");
+
+
+    $(window).on('scroll', () => {
+        // console.log(`scrollTop: ${$(window).scrollTop()}`);
+    })
 
     $('.band').on('click', function (e) {
         // band click logic goes 
-
-
+        scrollTween.scrollTrigger.start = 0;
         e.preventDefault();
         let targetElem = document.querySelector($(this).data('section')),
             y = targetElem;
-        if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
-            let totalScroll = scrollTween.scrollTrigger.end - scrollTween.scrollTrigger.start,
-                totalMovement = (sections.length - 1) * targetElem.offsetWidth;
-            console.log(totalMovement);
-            y = Math.round(scrollTween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
-        }
 
+        let totalScroll = scrollTween.scrollTrigger.end - scrollTween.scrollTrigger.start;
+
+        // let totalMovement = (sections.length - 1) * targetElem.offsetWidth;
+        let totalMovement = wrapperWidth - window.innerWidth;
+
+        y = (Math.round(scrollTween.scrollTrigger.start + ((bandTriggers[$(this).index()].scrollTrigger.markerEnd.offsetLeft - ($(this).index() * $(this).width())) / totalMovement) * totalScroll));
+
+
+
+        console.log(bandTriggers[$(this).index()].scrollTrigger.markerStart.offsetLeft);
+        console.log(y);
         gsap.to(window, {
+            ease: 'linear',
             scrollTo: {
                 y: y,
                 autoKill: false
             },
-            duration: 1
         });
     })
 
@@ -94,78 +109,80 @@ document.addEventListener('DOMContentLoaded', () => {
             end: 2000,
         }
     })
-    document.querySelectorAll('.section').forEach(el => {
-        gsap.to(el.querySelector('.caption'), {
-            x: 0,
-            y: 0,
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('.caption'),
-                start: 'top bottom',
-                end: '+=1000',
-                scrub: 1,
-            }
-        })
-
-        gsap.to(el.querySelector('.quote'), {
-            y: 0,
-            ease: 'none',
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('.quote'),
-                start: 'top bottom',
-                end: '+=20%',
-                scrub: 1,
-            }
-        })
-
-        gsap.to(el.querySelector('.nickname'), {
-            y: 0,
-            ease: 'none',
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('.nickname'),
-                start: 'top bottom',
-                end: '+=10%',
-                scrub: 1,
-            }
-        })
-        gsap.to(el.querySelector('.block'), {
-            x: 0,
-            ease: 'none',
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('.block'),
-                start: 'top bottom',
-                end: '+=' + window.innerWidth,
-                scrub: 1,
-            }
-        })
-        gsap.to(el.querySelector('img'), {
-            y: 0,
-            ease: 'none',
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('img'),
-                start: 'top bottom',
-                end: '+=50%',
-                scrub: 1,
-            }
-        })
-        gsap.to(el.querySelector('.huge-text'), {
-            y: 0,
-            ease: 'none',
-            scrollTrigger: {
-                containerAnimation: scrollTween,
-                trigger: el.querySelector('.huge-text'),
-                start: 'top bottom',
-                end: '+=100%',
-                scrub: 1,
-            }
-        })
 
 
-    })
+    // document.querySelectorAll('.section').forEach(el => {
+    //     gsap.to(el.querySelector('.caption'), {
+    //         x: 0,
+    //         y: 0,
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('.caption'),
+    //             start: 'top bottom',
+    //             end: '+=1000',
+    //             scrub: 1,
+    //         }
+    //     })
+
+    //     gsap.to(el.querySelector('.quote'), {
+    //         y: 0,
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('.quote'),
+    //             start: 'top bottom',
+    //             end: '+=20%',
+    //             scrub: 1,
+    //         }
+    //     })
+
+    //     gsap.to(el.querySelector('.nickname'), {
+    //         y: 0,
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('.nickname'),
+    //             start: 'top bottom',
+    //             end: '+=10%',
+    //             scrub: 1,
+    //         }
+    //     })
+    //     gsap.to(el.querySelector('.block'), {
+    //         x: 0,
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('.block'),
+    //             start: 'top bottom',
+    //             end: '+=' + window.innerWidth,
+    //             scrub: 1,
+    //         }
+    //     })
+    //     gsap.to(el.querySelector('img'), {
+    //         y: 0,
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('img'),
+    //             start: 'top bottom',
+    //             end: '+=50%',
+    //             scrub: 1,
+    //         }
+    //     })
+    //     gsap.to(el.querySelector('.huge-text'), {
+    //         y: 0,
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //             containerAnimation: scrollTween,
+    //             trigger: el.querySelector('.huge-text'),
+    //             start: 'top bottom',
+    //             end: '+=100%',
+    //             scrub: 1,
+    //         }
+    //     })
+
+
+    // })
 
 
 
